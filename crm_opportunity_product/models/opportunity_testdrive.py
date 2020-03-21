92045c8fc07e64723a4e7dcfca697aa6940937fa
tarikMch <t.mchichi@gmail.com>
2020-03-21 14:37:38 +0100

    fleet4

--- crm_opportunity_product/models/opportunity_testdrive.py
+++ crm_opportunity_product/models/opportunity_testdrive.py
@@ -20,7 +20,19 @@ class CrmLead(models.Model):
     _inherit = 'crm.lead'
     
     lead_testdrive_ids = fields.One2many('car.rental.contract','lead_id',string='Test Drive')
+    testdrive_count = fields.Integer(compute='_compute_testdrive_count', string="Test Drive Count")
 
+    @api.multi
+    def _compute_task_count(self):
+        count = 0
+        testdrive_ids = self.env['car.rental.contract'].search([])
+        for record in self:
+            if record:
+                for task_id in task_ids:
+                    if record.partner_id == testdrive_ids.customer_id:
+                        count=count+1 
+                        record.task_count = count
+        return True
 
 
     
